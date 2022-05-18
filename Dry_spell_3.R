7b629d768ccc209dce6c7cfaf22609f263e08d5f
Yacou <yacouba.ouedraogo@aims-senegal.org>
2022-05-18 08:29:47 +0100

    Update

--- Dry_spell_3.R
+++ Dry_spell_3.R
@@ -4,9 +4,9 @@
 
 
 
-prcp=Data$Precipitation
-Nb=10
-t=2.5
+# prcp=Data$Precipitation
+# Nb=10
+# t=2.5
 CDD_function=function(prcp,t,Nb) {
   
   prcp[length(prcp)+1]=10
@@ -42,37 +42,38 @@ for (i in 2:n) {
    CDD$spell[1]=0
    CDD$Count<-ifelse(CDD$spell>=Nb,1,0)
    S=max(CDD$spell)
-   return(S)
+   S10=sum(CDD$Count)
+   return(S10)
 }
 
-CDD_function(prcp = prcp,t=2.5,Nb=10)
+#CDD_function(prcp = prcp,t=2.5,Nb=10)
 
 
-
-
-
-
-
-All <- data.frame(#Country = character(),
-                  #Station = character(),
-                  #Lon=double(),
-                  #Lat=double(),
-                  #Date=character(),
-                  #Precipitation=double(),
-                  year=double(),
-                  Max_spell=double(),
-                  stringsAsFactors = FALSE)
-
-Year<-seq(1983,2021,1)
-k=2000
-for(k in Year){
-  Dat<-filter(Data,year==k)
-  max=CDD_function(prcp = Dat$Precipitation,t=2.5,Nb=10)
-  All=rbind(All,data.frame(k,max))
-}
-
-d1=rio::import("Data/Mangodara2.csv")
-colnames(d1)[1]=c('year')
-colnames(All)[1]=c('year')
-d2=merge(All,d1,by="year")
-d2$diff=d2$max-d2$V3         
+# 
+# 
+# 
+# 
+# 
+# All <- data.frame(#Country = character(),
+#                   #Station = character(),
+#                   #Lon=double(),
+#                   #Lat=double(),
+#                   #Date=character(),
+#                   #Precipitation=double(),
+#                   year=double(),
+#                   Max_spell=double(),
+#                   stringsAsFactors = FALSE)
+# 
+# Year<-seq(1983,2021,1)
+# k=2000
+# for(k in Year){
+#   Dat<-filter(Data,year==k)
+#   max=CDD_function(prcp = Dat$Precipitation,t=2.5,Nb=10)
+#   All=rbind(All,data.frame(k,max))
+# }
+# 
+# d1=rio::import("Data/Mangodara2.csv")
+# colnames(d1)[1]=c('year')
+# colnames(All)[1]=c('year')
+# d2=merge(All,d1,by="year")
+# d2$diff=d2$max-d2$V3         
